4ce9a36540ec52a105a7a9eeba567d2a9071239a
Anukarsh <anukarshr@gmail.com>
2021-09-15 20:51:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 console.log("This is a script file")
 
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+const phone = document.getElementById('phone')
+
+// console.log(name, email, phone)
+
